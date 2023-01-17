b3aa84b0eabf323c136e152655eccc8c3685a2f5
Razgriz <47901762+rrazgriz@users.noreply.github.com>
2023-01-16 19:18:46 -0500

    Add SwapGlobalTexture

---
+++ SwapGlobalTexture.cs
@@ -0,0 +1,43 @@
+﻿// SPDX-License-Identifier: MIT
+// Copyright 2023 Razgriz
+
+// Tool to set a global texture property to one of two textures. Useful for testing shader behavior with AudioLink present/not present.
+// Usage: Add to a GameObject in-scene. Use checkboxes to switch.
+
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SwapGlobalTexture : MonoBehaviour
+{
+    public Texture textureA, textureB;
+    public bool swapToA, swapToB;
+    public string texturePropertyToSet = "_AudioTexture";
+
+    void Start()
+    {
+        #if UNITY_EDITOR
+        if(textureA == null)
+        {
+            string[] findAudioLink = AssetDatabase.FindAssets("rt_audioLink t:rendertexture");
+            if(findAudioLink.Length == 1)
+                textureA = AssetDatabase.LoadAssetAtPath<RenderTexture>(AssetDatabase.GUIDToAssetPath(findAudioLink[0]));
+        }
+        #endif
+    }
+
+    void Update()
+    {
+        #if UNITY_EDITOR
+        if(swapToA || swapToB)
+        {
+            if(swapToA)
+                Shader.SetGlobalTexture(Shader.PropertyToID(texturePropertyToSet), textureA);
+            else if(swapToB)
+                Shader.SetGlobalTexture(Shader.PropertyToID(texturePropertyToSet), textureB);
+            
+            swapToA = swapToB = false;
+        }
+        #endif
+    }
+}
